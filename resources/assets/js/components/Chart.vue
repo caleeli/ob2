62a6c7b3e1b49050fd241eb6778d9fe89fbc51ec
david <davidcallizaya@gmai.com>
2017-04-27 06:27:39 -0400

    Chart Table

--- resources/assets/js/components/Chart.vue
+++ resources/assets/js/components/Chart.vue
@@ -2,20 +2,27 @@
     <div>
         <div class="btn-toolbar">
             <div class="btn-group">
-                <a href="javascript:void(0)" v-on:click="setType('bar')" :class="classType('bar')"><i class="fa fa-bar-chart"></i></a>
-                <a href="javascript:void(0)" v-on:click="setType('bar2')" :class="classType('bar2')"><i class="fa fa-bar-chart"></i></a>
-                <a href="javascript:void(0)" v-on:click="setType('horizontalBar')" :class="classType('horizontalBar')"><i class="fa fa-bars"></i></a>
-                <a href="javascript:void(0)" v-on:click="setType('area')" :class="classType('area')"><i class="fa fa-area-chart"></i></a>
-                <a href="javascript:void(0)" v-on:click="setType('line')" :class="classType('line')"><i class="fa fa-line-chart"></i></a>
-                <a href="javascript:void(0)" v-on:click="setType('pie')" :class="classType('pie')"><i class="fa fa-pie-chart"></i></a>
-                <a href="javascript:void(0)" v-on:click="setType('polarArea')" :class="classType('polarArea')"><i class="glyphicon glyphicon-cd"></i></a>
-                <a href="javascript:void(0)" v-on:click="setType('table')" :class="classType('table')"><i class="fa fa-table"></i></a>
+                <a v-for="chart in chartTypes" href="javascript:void(0)" v-on:click="setType(chart)" :class="classType(chart.type)"><i :class="chart.icon"></i></a>
             </div>
         </div>
         <div class="canvasOwner">
             <canvas></canvas>
         </div>
-        <div class="dataTable">
+        <div class="pv-data-table collapse">
+            <table>
+                <thead>
+                <tr v-for="(col,colI) in cols">
+                    <th v-for="row in rows"></th>
+                    <th v-for="(x,xi) in xs_label" v-if="isDifferentCol(xs_label, xi, colI)" :colspan="colspan(xs_label, xi, colI)">{{x[colI]}}</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="(yi,i) in ys">
+                    <th v-for="yLabel in ys_label[i]">{{yLabel}}</th>
+                    <td v-for="y in yi">{{Number(y).toLocaleString('es-419')}}</td>
+                </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </template>
@@ -25,6 +32,12 @@
         data:function() {
             return {
                 "chartType": "line",
+                "xs":[],
+                "ys":[],
+                "xs_label":[],
+                "ys_label":[],
+                "rows":[],
+                "cols":[],
             };
         },
         props:[
@@ -38,9 +51,31 @@
             }
         },
         methods: {
-            setType: function(type) {
-                this.chartType=type;
-                this.drawChart();
+            isDifferentCol: function (xs_label, i, colI) {
+                return i==0 ? true : xs_label[i][colI]!=xs_label[i-1][colI];
+            },
+            colspan: function (xs_label, i, colI) {
+                var colspan=0;
+                for(var j=i,l=xs_label.length;j<l;j++) {
+                    if (xs_label[j][colI]==xs_label[i][colI]) {
+                        colspan++;
+                    } else {
+                        return colspan;
+                    }
+                }
+                return colspan;
+            },
+            setType: function(chart) {
+                $(this.$el).find('.canvasOwner').addClass("collapse");
+                $(this.$el).find('.pv-data-table').addClass("collapse");
+                $(this.$el).find('.'+chart.element).removeClass("collapse");
+                if (chart.element==='canvasOwner') {
+                    this.chartType=chart.type;
+                    this.drawChart();
+                }
+                if(chart.element==='pv-data-table') {
+                    
+                }
             },
             classType: function(type) {
                 return this.chartType===type?'btn btn-primary':'btn btn-default';
@@ -85,6 +120,26 @@
                 var data = self.data;
                 $(self.$el).find(".canvasOwner").html("");
                 var maxNumCharts = MAX_ROWS;
+                self.xs.length=0;
+                self.ys.length=0;
+                self.xs_label.length=0;
+                self.ys_label.length=0;
+                self.rows.length=0;
+                self.cols.length=0;
+                self.model.cols.split(",").forEach(function(name) {
+                    if(name) self.cols.push(name);
+                });
+                self.rows.push('variable');
+                self.model.rows.split(",").forEach(function(name) {
+                    if(name) self.rows.push(name);
+                });
+                data.x.forEach(function(names){
+                    var xl=[];
+                    names.split("\x1b").forEach(function(name) {
+                        xl.push(name);
+                    });
+                    self.xs_label.push(xl);
+                });
                 for(var rowId in data.series) {
                     addChart(rowId);
                     maxNumCharts--;
@@ -108,6 +163,12 @@
                     }
                     var axis=["y-axis-2","y-axis-1"];
                     for(var a in data.series[rowId]) {
+                        var yl=[a];
+                        rowId.split("\x1b").forEach(function(name){
+                            yl.push(name);
+                        });
+                        self.ys.push(data.series[rowId][a]);
+                        self.ys_label.push(yl);
                         lineChartData.datasets.push({
                             label: a,
                             borderColor: colors.pop(),
@@ -271,9 +332,7 @@
                     
                 } 
             },
-            refreshTable: function () {
-                /*$(this.$el).find('.dataTable')
-                    .html('<table class="display" width="100%"></table>');*/
+            /*refreshTable: function () {
                 var data=[];
                 for(var serie in self.data.series) {
                     for(var a in data.series[serie]) {
@@ -300,10 +359,52 @@
                         { title: "Salary" }
                     ]
                 } );
-            },
+            },*/
         },
         mounted() {
             var self = this;
+            self.chartTypes = {
+                "bar": {
+                    type:"bar",
+                    element: "canvasOwner",
+                    icon: "fa fa-bar-chart",
+                },
+                "bar2": {
+                    type: "bar2",
+                    element: "canvasOwner",
+                    icon: "fa fa-bar-chart",
+                },
+                "horizontalBar": {
+                    type: "horizontalBar",
+                    element: "canvasOwner",
+                    icon: "fa fa-bars",
+                },
+                "area": {
+                    type: "area",
+                    element: "canvasOwner",
+                    icon: "fa fa-area-chart",
+                },
+                "line": {
+                    type: "line",
+                    element: "canvasOwner",
+                    icon: "fa fa-line-chart",
+                },
+                "pie": {
+                    type: "pie",
+                    element: "canvasOwner",
+                    icon: "fa fa-pie-chart",
+                },
+                "polarArea": {
+                    type: "polarArea",
+                    element: "canvasOwner",
+                    icon: "glyphicon glyphicon-cd",
+                },
+                "table": {
+                    type: "table",
+                    element: "pv-data-table",
+                    icon: "fa fa-table",
+                },
+            };
             self.refresh();
             this.$root.$on('changed', function(element){
                 if(typeof element.id!=='undefined') {
