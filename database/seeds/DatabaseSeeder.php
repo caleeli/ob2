2f74aa05d224a9b844fe5ce8634d185425b0cf22
davidcallizaya <davidcallizaya@192.168.11.22>
2017-04-28 17:09:44 -0400

    Installer

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(ReportsFoldersSeeder::class);
+         $this->call(UsersTableSeeder::class);
     }
 }
