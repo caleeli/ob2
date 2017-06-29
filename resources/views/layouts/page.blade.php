37a04e2064cf2cae87d4c45ad66278e2c934e53a
davidcallizaya <davidcallizaya@192.168.11.22>
2017-06-29 01:05:52 -0400

    tabla empresas

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -12,7 +12,6 @@
         <link rel="stylesheet" type="text/css" href="css/buttons.dataTables.min.css">
         <link rel="stylesheet" type="text/css" href="bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="bower_components/datatables.net-responsive-bs/css/responsive.bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" href="bower_components/jstree/dist/themes/default/style.min.css">
         <link rel="stylesheet" type="text/css" href="bower_components/datatables-fixedcolumns/css/fixedColumns.bootstrap.scss">
         <link rel="stylesheet" type="text/css" href="css/test.css?t=<?=filemtime(public_path('css/test.css'))?>">
         <!-- Styles -->
@@ -28,17 +27,13 @@
     <body>
 @yield('content')
         <script type="text/javascript" charset="utf8" src="bower_components/jq-ajax-progress/src/jq-ajax-progress.min.js"></script>
+
         <script type="text/javascript" charset="utf8" src="bower_components/datatables.net/js/jquery.dataTables.js"></script>
         <script type="text/javascript" charset="utf8" src="bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
-
         <script type="text/javascript" charset="utf8" src="bower_components/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
-        <script type="text/javascript" charset="utf8" src="js/jszip.min.js"></script>
-        <script type="text/javascript" charset="utf8" src="js/pdfmake.min.js"></script>
-        <script type="text/javascript" charset="utf8" src="js/vfs_fonts.js"></script>
         <script type="text/javascript" charset="utf8" src="bower_components/datatables.net-buttons/js/buttons.colVis.min.js"></script>
         <script type="text/javascript" charset="utf8" src="bower_components/datatables.net-buttons/js/buttons.html5.min.js"></script>
         <script type="text/javascript" charset="utf8" src="bower_components/datatables.net-buttons/js/buttons.print.min.js"></script>
-
         <script type="text/javascript" charset="utf8" src="bower_components/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
         <script type="text/javascript" charset="utf8" src="bower_components/datatables.net-responsive-bs/js/responsive.bootstrap.min.js"></script>
         <script type="text/javascript" charset="utf8" src="bower_components/datatables-fixedcolumns/js/dataTables.fixedColumns.js"></script>
