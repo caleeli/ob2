7a97f957c6df56c445a016addaa680e958e89931
David Callizaya <david.calizaya@gmail.com>
2017-08-16 12:06:35 -0400

    fix database encoding;

--- public/HojaRuta/connection.php
+++ public/HojaRuta/connection.php
@@ -3,6 +3,6 @@ $host = 'localhost';
 $db = 'sistema';
 $username = 'root';
 $password = 'root';
-$connection = new PDO("mysql:dbname=$db;host=$host", $username, $password);
+$connection = new PDO("mysql:dbname=$db;host=$host;charset=utf8", $username, $password );
 
 return $connection;
