b8ab8a192fed599b3a3fe591e88a3f5db37038be
davidcallizaya <davidcallizaya@192.168.11.22>
2017-09-21 10:10:02 -0400

    upgrade js css

--- public/js/server.js
+++ public/js/server.js
@@ -1,6 +1,6 @@
 var API_SERVER='';
-document.writeln('\n<link href="/css/app.css?v0.40" rel="stylesheet">\n');
-document.writeln('\n<script src="/js/admin.js?v0.40"></script>\n');
+document.writeln('\n<link href="/css/app.css?v0.41" rel="stylesheet">\n');
+document.writeln('\n<script src="/js/admin.js?v0.41"></script>\n');
 document.writeln('\n<script type="text/javascript" charset="utf8" src="/bower_components/jq-ajax-progress/src/jq-ajax-progress.min.js"></script>\n');
 document.writeln('\n<script type="text/javascript" src="/bower_components/datatables.net/js/jquery.dataTables.js"></script>\n');
 document.writeln('\n<script type="text/javascript" src="/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>\n');
