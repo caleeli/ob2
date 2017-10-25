13a45d23bed5e6b268fa39f46e6cd62d6108ee49
Angela <angelitacc27@gmail.com>
2017-10-24 20:49:24 -0700

    numero de registros

--- public/HojaRuta/select.php
+++ public/HojaRuta/select.php
@@ -3,7 +3,7 @@
 $connection = require('connection.php');
 
 if (empty($_REQUEST['filter'])) {
-    $stmt = $connection->prepare('select * from hoja_ruta order by fecha desc limit 15');
+    $stmt = $connection->prepare('select * from hoja_ruta order by fecha desc limit 50');
     $stmt->execute();
     $res =  $stmt->fetchAll();
 
@@ -11,7 +11,7 @@ if (empty($_REQUEST['filter'])) {
     $stmt = $connection->prepare('select * from hoja_ruta where '
         . 'fecha like :filter or referencia like :filter or procedencia like :filter or '
         . 'nro_de_control like :filter or anexo_hojas like :filter or '
-        . 'destinatario like :filter or conclusion like :filter order by fecha desc limit 15');
+        . 'destinatario like :filter or conclusion like :filter order by fecha desc limit 50');
     $stmt->execute([
         'filter' => '%'.$_REQUEST['filter'].'%'
     ]);
