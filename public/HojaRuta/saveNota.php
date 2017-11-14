412128fa139238bed97b8a3d951c64698bb681e4
davidcallizaya <davidcallizaya@192.168.11.22>
2017-11-14 10:14:31 -0400

    debug note

--- public/HojaRuta/saveNota.php
+++ public/HojaRuta/saveNota.php
@@ -41,9 +41,32 @@ if (empty($_REQUEST['id'])) {
         $_REQUEST['fojas_recepcion'],
     ]);
 } else {
+    ini_set("display_errors", "on");
+    error_reporting(E_ALL);
     $stmt = $connection->prepare('update notas set hoja_de_ruta=?, fecha_emision=?, nro_nota=?, reiterativa=?, fecha_entrega=?, entidad_empresa=?, nombre_apellidos=?, cargo=?, referencia=?, dias=?, retraso=?, hoja_de_ruta_recepcion=?, fecha_recepcion=?, nro_nota_recepcion=?, remitente_recepcion=?, referencia_recepcion=?, fojas_recepcion=? '
         .' where id = ?');
-
+    echo 'update notas set hoja_de_ruta=?, fecha_emision=?, nro_nota=?, reiterativa=?, fecha_entrega=?, entidad_empresa=?, nombre_apellidos=?, cargo=?, referencia=?, dias=?, retraso=?, hoja_de_ruta_recepcion=?, fecha_recepcion=?, nro_nota_recepcion=?, remitente_recepcion=?, referencia_recepcion=?, fojas_recepcion=? '
+        .' where id = ?';
+    var_dump([
+        $_REQUEST['hoja_de_ruta'],
+        $_REQUEST['fecha_emision'],
+        $_REQUEST['nro_nota'],
+        $_REQUEST['reiterativa'],
+        empty($_REQUEST['fecha_entrega'])?date('Y-m-d'):$_REQUEST['fecha_entrega'],
+        $_REQUEST['entidad_empresa'],
+        $_REQUEST['nombre_apellidos'],
+        $_REQUEST['cargo'],
+        $_REQUEST['referencia'],
+        $_REQUEST['dias'],
+        $_REQUEST['retraso'],
+        $_REQUEST['hoja_de_ruta_recepcion'],
+        $_REQUEST['fecha_recepcion'],
+        $_REQUEST['nro_nota_recepcion'],
+        $_REQUEST['remitente_recepcion'],
+        $_REQUEST['referencia_recepcion'],
+        $_REQUEST['fojas_recepcion'],
+        $_REQUEST['id'],
+    ]);
     $stmt->execute([
         $_REQUEST['hoja_de_ruta'],
         $_REQUEST['fecha_emision'],
