7986956fcfc5a4f1799fc3030bbdfd356f1b719d
davidcallizaya <davidcallizaya@gmail.com>
2018-03-08 23:09:16 -0400

    missing file

---
+++ app/Models/SaveUserTrait.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Exception;
+
+/**
+ * SaveUser
+ *
+ */
+trait SaveUserTrait
+{
+
+    /**
+     * Boot the soft deleting trait for a model.
+     *
+     * @return void
+     */
+    public static function boot()
+    {
+        parent::boot();
+        static::saving(function ($model) {
+            try {
+                $header = request()->header('Authorization');
+                list($auth, $user) = explode(' ', $header);
+                $model->usuario_abm_id = $user;
+            } catch (Exception $e) {
+                error_log('Unable to obtain authorization: ' . $e->getMessage());
+            }
+        });
+        static::deleting(function ($model) {
+            try {
+                $header = request()->header('Authorization');
+                list($auth, $user) = explode(' ', $header);
+                $model->usuario_abm_id = $user;
+                $model->save();
+            } catch (Exception $e) {
+                error_log('Unable to obtain authorization: ' . $e->getMessage());
+            }
+        });
+    }
+}
