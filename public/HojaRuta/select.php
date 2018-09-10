e4ac6c1543ddbabb1dc07f45cd5f1c65a56d1465
David Callizaya <davidcallizaya@gmail.com>
2018-09-10 14:07:48 -0400

    restore query

--- public/HojaRuta/select.php
+++ public/HojaRuta/select.php
@@ -3,20 +3,16 @@
 $connection = require('connection.php');
 $tipo = empty($_REQUEST['tipo']) ? '%' : $_REQUEST['tipo'];
 
-$esElUltimoDestinatario = 'derivacion.id = (select max(id) from derivacion d2 where d2.hoja_ruta_id=hoja_ruta.id)';
 if (empty($_REQUEST['filter'])) {
-    $stmt = $connection->prepare('select hoja_ruta.*, derivacion.destinatario from hoja_ruta '
-        . 'left join derivacion on (derivacion.hoja_ruta_id=hoja_ruta.id and '.$esElUltimoDestinatario.') '
-        . 'where tipo like "' . $tipo . '" order by id desc limit 50');
+    $stmt = $connection->prepare('select * from hoja_ruta where tipo like "' . $tipo . '" order by id desc limit 50');
     $stmt->execute();
     $res =  $stmt->fetchAll();
 
 } else {
-    $stmt = $connection->prepare('select hoja_ruta.*, derivacion.destinatario from hoja_ruta '
-        . 'left join derivacion on (derivacion.hoja_ruta_id=hoja_ruta.id and '.$esElUltimoDestinatario.') '
-        . 'where (hoja_ruta.fecha like :filter or hoja_ruta.referencia like :filter or hoja_ruta.procedencia like :filter or '
-        . 'hoja_ruta.nro_de_control like :filter or hoja_ruta.anexo_hojas like :filter or '
-        . 'derivacion.destinatario like :filter or hoja_ruta.conclusion like :filter or hoja_ruta.numero like :exacto) and hoja_ruta.tipo like "'
+    $stmt = $connection->prepare('select * from hoja_ruta where '
+        . '(fecha like :filter or referencia like :filter or procedencia like :filter or '
+        . 'nro_de_control like :filter or anexo_hojas like :filter or '
+        . 'destinatario like :filter or conclusion like :filter or numero like :exacto) and tipo like "'
         . $tipo . '" order by id desc limit 50');
     $stmt->execute([
         'filter' => '%'.$_REQUEST['filter'].'%',
