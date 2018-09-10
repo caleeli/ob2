16d383181ce317abecc8f19dadfe02bdbb724948
David Callizaya <davidcallizaya@gmail.com>
2018-09-10 09:18:55 -0400

    remove connection php

--- public/HojaRuta/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = '127.0.0.1';
-$db = 'sistema';
-$username = 'root';
-$password = '';
-$connection = new PDO("mysql:dbname=$db;host=$host;charset=utf8", $username, $password );
-
-return $connection;
