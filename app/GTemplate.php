09dddef2d92a97bf43ebd0478a38dd1e34fa8d12
David Callizaya <davidcallizaya@gmail.com>
2018-11-06 12:17:06 -0400

    Modo vista con variables array.

--- app/GTemplate.php
+++ app/GTemplate.php
@@ -89,7 +89,7 @@ class GTemplate
         $html .= '<script>var variables = ' . json_encode($variables) . ';parent && parent.app && parent.app.variablesCargadas ? parent.app.variablesCargadas(variables):null;</script>';
         return $html;
     }
-    
+
     private function addMultipleButtons($html)
     {
         return preg_replace_callback(
@@ -103,6 +103,120 @@ class GTemplate
         );
     }
 
+    /**
+     * Remove the add buttons from view.
+     *
+     * @param string $html
+     *
+     * @return string
+     */
+    private function removeMultipleButtons($html)
+    {
+        return preg_replace_callback(
+            '/\{\+([\w\.]+)\}/',
+            function ($match) {
+                $plural = $this->plural($match[1]);
+                return '';
+            },
+            $html
+        );
+    }
+
+    private function parseMultipleRows($html, $valores, &$variables, &$multiples, $loader='loadHTML') {
+        $html = preg_replace_callback(self::REGEXP_VARIABLE,
+                                      function ($match) use($valores, &$variables) {
+            $varName = $match[1];
+            if (strpos($varName, '.')>-1) {
+                $names = explode('.', $varName);
+                $last = count($names) - 1;
+                $ref = '$variables';
+                $refRef = '$variables';
+                $valRef = '$valores';
+                foreach($names as $i=>$singular) {
+                    if ($i === 0) {
+                        $plural = $this->plural($singular);
+                        $ref.='['. var_export($plural, true).'][0]';
+                        $refRef.='['. var_export($plural, true).']';
+                        $valRef.='['. var_export($plural, true).']';
+                        $child = $singular;
+                        $owner = $last > 1 ? $singular : $plural;
+                    } elseif ($i < $last) {
+                        $plural = $this->plural($singular);
+                        $ref.='['. var_export($plural, true).'][0]';
+                        $child = $singular;
+                        if ($i < $last - 1) {
+                            $owner = $singular;
+                        } else {
+                            $owner = $owner . '.' . $plural;
+                        }
+                    } else {
+                        $ref.='['. var_export($singular, true).']';
+                        $varName = $child . '.' . $singular;
+                    }
+                }
+                eval('if (isset(' . $valRef . ') && !isset(' . $refRef . ')) ' . $refRef . '= ' . $valRef . ';');
+                eval('if (!isset(' . $ref . ')) ' . $ref . '= "";');
+            } else {
+                //$variables[$varName] = isset($valores[$varName]) ? $valores[$varName] : '';
+                $owner = null;
+            }
+            $params = $match[3];
+            if ($params && strpos($params, '|')!==false) {
+                $params = htmlentities(json_encode(explode('|', $params)), ENT_QUOTES);
+            }
+            return $owner ? "<span w-owner='{$owner}' w-child='{$child}'>{$match[0]}</span>" : $match[0];
+        }, $html);
+        $dom = new \DomDocument;
+        $dom->$loader($html);
+        $xpath = new \DomXpath($dom);
+        $trs = [];
+        $changed = true;
+        while($changed) {
+            $elements = $xpath->query("//span[@w-owner]");
+            $changed = false;
+            foreach ($elements as $element) {
+                $plural = $element->getAttribute("w-owner");
+                $singular = $element->getAttribute("w-child");
+                $element->removeAttribute("w-owner");
+                $element->removeAttribute("w-child");
+                $tr = $element->parentNode;
+                while ($tr && strtoupper($tr->nodeName) !== 'TR') {
+                    $tr = $tr->parentNode;
+                }
+                if ($tr && $tr !== $dom->firstChild) {
+                    $tr->setAttribute('w-plural', $plural);
+                    $tr->setAttribute('w-singular', $singular);
+                    $this->renderFoEach($tr, $singular, $plural, $valores, $variables, $multiples);
+                    $changed = true;
+                    break;
+                }
+            }
+        }
+        return $dom->saveHTML();
+    }
+    
+    private function renderFoEach(\DOMElement $tr, $singular, $plural, $valores, &$variables, &$multiples)
+    {
+        $tr->removeAttribute('w-plural');
+        $tr->removeAttribute('w-singular');
+        $htmlBase = $tr->ownerDocument->saveXML($tr);
+        $refNode = $tr->nextSibling;
+        foreach(array_get($valores, $plural, []) as $fila) {
+            $valores2 = $valores;
+            array_set($valores2, str_singular($plural), $fila);
+            $html = $this->parseMultipleRows($htmlBase, $valores2, $variables, $multiples, 'loadXML');
+            $html =  $this->parseInnerValores($valores2, $html, $variables, $multiples);
+            $container = new \DOMDocument;
+            $container->loadXML($html);
+            $tr->parentNode->insertBefore(
+                $tr->ownerDocument->importNode($container->documentElement, true),
+                $refNode
+            );
+        }
+        $parent = $tr->parentNode;
+        $tr->parentNode->removeChild($tr);
+    }
+    
     private function plural($singular)
     {
         $lastLetter = strtolower(substr($singular, -1));
@@ -111,9 +225,18 @@ class GTemplate
 
     public function parseValores(array $valores = [])
     {
-        $multiples = [];
         $variables = [];
+        $multiples = [];
         $html = str_replace(array_keys(self::SHORT_CUTS), array_values(self::SHORT_CUTS), $this->rawContent);
+        $html = $this->parseMultipleRows($html, $valores, $variables, $multiples);
+        $html =  $this->parseInnerValores($valores, $html, $variables, $multiples);
+        $html = $this->removeMultipleButtons($html);
+        $html .= '<script>var variables = ' . json_encode($variables) . ';parent && parent.app && parent.app.variablesCargadas ? parent.app.variablesCargadas(variables):null;</script>';
+        return $html;
+    }
+
+    public function parseInnerValores(array $valores = [], $html, &$variables, &$multiples)
+    {
         $html = preg_replace_callback(self::REGEXP_VARIABLE,
                                       function ($match) use($valores, &$variables, &$multiples) {
             $varName = $match[1];
@@ -127,7 +250,7 @@ class GTemplate
             if ($params && strpos($params, '|')!==false) {
                 $params = htmlentities(json_encode(explode('|', $params)), ENT_QUOTES);
             }
-            $valor = $variables[$varName];
+            $valor = array_get($valores, $varName, '');
             if ($match[2] === 'lista') {
                 $lista = $this->loadLista($match[3]);
                 $valor = @$lista[$valor];
