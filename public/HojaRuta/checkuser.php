6adb296cb3dd1d7c7d96af4aa1d9b8ac7325c3a3
David Callizaya <davidcallizaya@gmail.com>
2019-03-19 18:16:18 -0400

    wv

--- public/HojaRuta/checkuser.php
+++ public/HojaRuta/checkuser.php
@@ -3,7 +3,7 @@ session_start();
 $username = $_REQUEST['username'];
 $password = $_REQUEST['password'];
 
-if ($username === 'mary' && $password === 'm2018') {
+if (($username === 'mary' && $password === 'm2018') || ($username === 'wvargas' && $password === 'wvargas123')) {
     $_SESSION['hr_user'] = $username;
     header('Location: index.php');
 } else {
