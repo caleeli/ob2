1f27bdc473ebf4d4d51e9e6bc50a213558383819
David Callizaya <davidcallizaya@gmail.com>
2019-04-04 21:10:31 -0400

    firmas de auditoria

---
+++ app/Models/UserAdministration/Lafirma.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Models\UserAdministration;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+use App\Models\SaveUserTrait;
+
+
+class Lafirma extends Model
+{
+    use SoftDeletes, Notifiable, SaveUserTrait;
+    protected $table = 'adm_firmas';
+    protected $fillable = array (
+      0 => 'cod_firma',
+      1 => 'nombre_empresa',
+    );
+    protected $attributes = array (
+      'cod_firma' => '',
+      'nombre_empresa' => '',
+    );
+    protected $casts = array (
+      'cod_firma' => 'string',
+      'nombre_empresa' => 'string',
+    );
+    protected $events = array (
+    );
+
+}
