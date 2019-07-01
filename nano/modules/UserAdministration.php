71ad9e12c9105ee87805e90059229449885c4d09
David Callizaya <davidcallizaya@gmail.com>
2019-07-01 08:58:04 -0400

    fix titulo supervisor

--- nano/modules/UserAdministration.php
+++ nano/modules/UserAdministration.php
@@ -931,7 +931,7 @@
                     }),
                     new Module.Model.BelongsTo({
                         "name": "supervisor",
-                        "label": "Supervisor Evaluador",
+                        "label": "Supervisor",
                         "model": "user",
                         "textField": function(data){return data?data.nombres + ' ' +data.apellidos:''},
                         "ui": "select",
@@ -1100,7 +1100,7 @@
                     }),
                     new Module.Model.BelongsTo({
                         "name": "supervisor",
-                        "label": "Supervisor",
+                        "label": "Supervisor Evaluador",
                         "model": "user",
                         "textField": function(data){return data?data.nombres + ' ' +data.apellidos:''},
                         "ui": "select",
--- public/js/server.js
+++ public/js/server.js
@@ -1,6 +1,6 @@
 var API_SERVER='';
-document.write('<link href="/css/app.css?v0.181" rel="stylesheet">');
-document.write('<script src="/js/admin.js?v0.181"></script>');
+document.write('<link href="/css/app.css?v0.182" rel="stylesheet">');
+document.write('<script src="/js/admin.js?v0.182"></script>');
 document.write('<script type="text/javascript" charset="utf8" src="/bower_components/jq-ajax-progress/src/jq-ajax-progress.min.js"></script>');
 document.write('<script type="text/javascript" src="/bower_components/datatables.net/js/jquery.dataTables.js"></script>');
 document.write('<script type="text/javascript" src="/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>');
