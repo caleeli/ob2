e092a2821d392b2409239768fba6d3381c5e7a31
David Callizaya <davidcallizaya@gmail.com>
2021-04-06 20:14:33 -0400

    Corregir sql paginacion

--- public/HojaRuta/select.php
+++ public/HojaRuta/select.php
@@ -4,17 +4,15 @@ $connection = require('connection.php');
 $tipo = empty($_REQUEST['tipo']) ? '%' : $_REQUEST['tipo'];
 $per_page = empty($_REQUEST['per_page']) ? '10' : $_REQUEST['per_page'];
 $page = empty($_REQUEST['page']) ? '1' : $_REQUEST['page'];
+$per_page = $per_page * 1;
 $offset = ($page - 1) * $per_page;
 
 $esElUltimoDestinatario = 'derivacion.id = (select max(id) from derivacion d2 where d2.hoja_ruta_id=hoja_ruta.id)';
 if (empty($_REQUEST['filter'])) {
     $stmt = $connection->prepare('select hoja_ruta.*, derivacion.destinatario as derivacion_destinatario from hoja_ruta '
         . 'left join derivacion on (derivacion.hoja_ruta_id=hoja_ruta.id and '.$esElUltimoDestinatario.') '
-        . 'where tipo like "' . $tipo . '" order by id desc limit :per_page offset :offset');
-    $stmt->execute([
-        'per_page' => $per_page,
-        'offset' => $offset,
-    ]);
+        . 'where tipo like "' . $tipo . '" order by id desc limit ' . $per_page . ' offset ' . $offset);
+    $stmt->execute();
     $res =  $stmt->fetchAll();
 
 } else {
@@ -23,12 +21,10 @@ if (empty($_REQUEST['filter'])) {
         . 'where (hoja_ruta.fecha like :filter or hoja_ruta.referencia like :filter or hoja_ruta.procedencia like :filter or '
         . 'hoja_ruta.nro_de_control like :filter or hoja_ruta.anexo_hojas like :filter or '
         . 'derivacion.destinatario like :filter or hoja_ruta.conclusion like :filter or hoja_ruta.numero like :exacto) and hoja_ruta.tipo like "'
-        . $tipo . '" order by id desc limit :per_page offset :offset');
+        . $tipo . '" order by id desc limit ' . $per_page . ' offset ' . $offset);
     $stmt->execute([
         'filter' => '%'.$_REQUEST['filter'].'%',
         'exacto' => $_REQUEST['filter'],
-        'per_page' => $per_page,
-        'offset' => $offset,
     ]);
     $res =  $stmt->fetchAll();
 }
